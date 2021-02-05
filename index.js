89ac4f5c1d382ea08e81bd5818d503df769a06ec
Christina Williams <christinaxtwilliams@gmail.com>
2021-02-05 14:03:12 -0500

    pauseButton

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class Timer {
   constructor(durationInput, startButton, pauseButton) {
     this.durationInput = durationInput;
     this.startButton = startButton;
-    
+    this.pauseButton = pauseButton;
 
   }
 }
