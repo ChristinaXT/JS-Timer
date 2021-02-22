6ed713b75dd4428f62c982ab1a2f8e244231850d
Christina Williams <christinaxtwilliams@gmail.com>
2021-02-22 15:37:44 -0500

    circle

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
