d802ba37c04e16eceead99cc37a40e145fcf8a75
Christina Williams <christinaxtwilliams@gmail.com>
2021-02-22 15:38:41 -0500

    circle

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log('Timer started');
