1db138f80ed3796783ff4baee644b995089a8c6a
Christina Williams <christinaxtwilliams@gmail.com>
2021-02-25 15:32:21 -0500

    formula

--- timer.js
+++ timer.js
@@ -19,7 +19,7 @@ start = () => {
     this.onStart(this.timeRemaining);
   }
   this.tick();
-  this.interval = setInterval(this.tick, 50);
+  this.interval = setInterval(this.tick, 20);
 };
 
 pause = () => {
@@ -32,7 +32,7 @@ pause = () => {
         this.onComplete();
       }
     } else {
-      this.timeRemaining = this.timeRemaining - .05;
+      this.timeRemaining = this.timeRemaining - .02;
       if (this.onTick) {
         this.onTick(this.timeRemaining);
       }
